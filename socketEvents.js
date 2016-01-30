19ff8e13f0447d3f3ca2c9df107d621b9248fdec
dan <dynamicdan100@yahoo.com>
2016-01-30 11:28:17 -0500

    fixed updating client to sock tables

--- socketEvents.js
+++ socketEvents.js
@@ -1,15 +1,19 @@
 console.log("socketEvents.js loaded")
 module.exports = function(io){
-	console.log("socket events have been added");
+	console.log("socket events have been added 1.1");
 
 	io.on("connection",function(socket){
 		socket.on("newChatListener",function(data){
 			console.log(user2Sock);
 			console.log(sock2User);
+			//not using this because it is an insecure idea
 		});
 		socket.on("disconnect",function(){
+			console.log("disconnecting");
+			console.log(username);
 			var username = sock2User[socket.id];
-			if(! typeof(username) == "undefined"){
+			if( typeof(username) !== "undefined"){
+				console.log("updating user2sock and sock2user objects")
 				delete sock2User[socket.id];
 				delete user2Sock[username][socket.id];
 			}
